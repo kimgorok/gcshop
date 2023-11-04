dbf60e8ea0405c8bb14591e78823514e94526334
kimgorok <kjyse2@naver.com>
2023-11-04 15:23:59 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+//express와 views 정의
+const express = require("express");
+const app = express();
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+//사용자 정의 모듈
+var rootRouter = require("./router/rootRouter");
+var authRouter = require("./router/authRouter");
+var codeRouter = require("./router/codeRouter");
+// var personRouter = require("./router/personRouter");
+var merchandiseRouter = require("./router/merchandiseRouter");
+
+// 세션 모듈, 세션 DB 저장 모듈
+var session = require("express-session");
+var MySqlStore = require("express-mysql-session")(session);
+var options = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  database: "webdb2023",
+};
+var sessionStore = new MySqlStore(options);
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+  })
+);
+
+// body 파서 모듈
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 라우터 호출
+app.use("/", rootRouter);
+app.use("/auth", authRouter);
+app.use("/code", codeRouter);
+// app.use("/person", personRouter);
+app.use("/merchandise", merchandiseRouter);
+//app.use('/board',boardRouter);
+
+//정적 파일 폴더 지정
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000"));
---
+++ router/rootRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var router = express.Router()
+
+var shop = require('../lib/shop');
+
+
+router.get('/',(req, res)=>{
+
+    shop.home(req, res);
+}); 
+
+module.exports = router;
+
